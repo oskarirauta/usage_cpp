b75c524e540a466e7630fd1ea08214cfca0f89cd
Oskari Rauta <oskari.rauta@gmail.com>
2024-12-07 19:58:37 +0200

    update usage.cpp
    Signed-off-by: Oskari Rauta <oskari.rauta@gmail.com>

--- src/usage.cpp
+++ src/usage.cpp
@@ -3,6 +3,7 @@
 #include <type_traits>
 #include <stdexcept>
 #include <algorithm>
+#include "common.hpp"
 
 #include "usage.hpp"
 
@@ -13,29 +14,29 @@ static std::string line_feed(const std::stringstream& ss) {
 	return "\n";
 }
 
-args_t::args_t(const int argc, char **argv) {
+usage_t::args_t::args_t(const int argc, char **argv) {
 
 	this -> _cmd = argv[0];
 	for ( int i = 1; i < argc; i++ )
 		this -> _vec.push_back(argv[i]);
 }
 
-size_t args_t::size() const {
+size_t usage_t::args_t::size() const {
 
 	return this -> _vec.size();
 }
 
-bool args_t::empty() const {
+bool usage_t::args_t::empty() const {
 
 	return this -> _vec.empty();
 }
 
-std::string args_t::cmd() const {
+std::string usage_t::args_t::cmd() const {
 
 	return this -> _cmd;
 }
 
-std::string args_t::operator [](size_t index) const {
+std::string usage_t::args_t::operator [](size_t index) const {
 
 	if ( index >= this -> _vec.size())
 		throw std::runtime_error("argument " + std::to_string(index) + " is out of bounds(" + std::to_string(this -> _vec.size()) + ")");
@@ -43,12 +44,65 @@ std::string args_t::operator [](size_t index) const {
 	return this -> _vec[index];
 }
 
-usage_t::result_t::operator bool() {
+usage_t::result_t::operator bool() const {
 	return this -> enabled;
 }
 
-usage_t::result_t::operator std::string() {
+usage_t::result_t::operator std::string() const {
 	return this -> value;
+}
+
+bool usage_t::result_t::boolValue() const {
+	return common::to_lower(common::trim_ws(this -> value)) == "true" ? true : false;
+}
+
+std::string usage_t::result_t::stringValue() const {
+	return this -> value;
+}
+
+long usage_t::result_t::intValue() const {
+
+	long res = 0;
+
+	try {
+		res = std::stol(this -> value);
+	} catch (...) {
+		res = 0;
+	}
+	return res;
+}
+
+double usage_t::result_t::doubleValue() const {
+
+	double res = 0;
+
+	try {
+		res = std::stod(this -> value);
+	} catch (...) {
+		res = 0;
+	}
+	return res;
+}
+
+double usage_t::result_t::floatValue() const {
+	return this -> doubleValue();
+}
+
+long usage_t::result_t::hexValue() const {
+
+	long res = 0;
+
+	try {
+		res = std::stol(this -> value, nullptr, 0);
+	} catch (...) {
+		res = 0;
+	}
+	return res;
+}
+
+std::string usage_t::name() const {
+
+	return this -> info.name;
 }
 
 std::string usage_t::cmd() const {
@@ -60,64 +114,64 @@ std::string usage_t::title() const {
 
 	std::stringstream ss;
 
-	if ( !this -> name.empty()) {
+	if ( !this -> info.name.empty()) {
 
-		ss << this -> name;
-		if ( !this -> version.empty()) {
+		ss << this -> info.name;
+		if ( !this -> info.version.empty()) {
 
-			if ( this -> version.front() != '\n' )
+			if ( this -> info.version.front() != '\n' )
 				ss << ' ';
 
-			if ( !this -> version_title.empty())
-				ss << this -> version_title << this -> version;
-			else ss << this -> version;
+			if ( !this -> info.version_title.empty())
+				ss << this -> info.version_title << this -> info.version;
+			else ss << this -> info.version;
 
 		}
 
-	} else if ( !this -> version.empty()) {
+	} else if ( !this -> info.version.empty()) {
 
-		std::string t = this -> version_title;
+		std::string t = this -> info.version_title;
 		while ( !t.empty() && ( std::isspace(t.front()) || t.front() == '\n' || t.front() == '\r' ))
 			t.erase(0, 1);
 
 		if ( !t.empty())
-			ss << t << this -> version;
-		else ss << this -> version;
+			ss << t << this -> info.version;
+		else ss << this -> info.version;
 	}
 
-	if ( !this -> author.empty()) ss << line_feed(ss) << this -> author_title << this -> author;
-	if ( !this -> copyright.empty()) ss << line_feed(ss) << this -> copyright_title << this -> copyright;
+	if ( !this -> info.author.empty()) ss << line_feed(ss) << this -> info.author_title << this -> info.author;
+	if ( !this -> info.copyright.empty()) ss << line_feed(ss) << this -> info.copyright_title << this -> info.copyright;
 
 	return ss.str();
 }
 
-std::string usage_t::version_info() const {
+std::string usage_t::version() const {
 
 	std::stringstream ss;
 
-	if ( !this -> name.empty()) {
+	if ( !this -> info.name.empty()) {
 
-		ss << this -> name;
-		if ( !this -> version.empty()) {
+		ss << this -> info.name;
+		if ( !this -> info.version.empty()) {
 
-			if ( this -> version.front() != '\n' )
+			if ( this -> info.version.front() != '\n' )
 				ss << ' ';
 
-			if ( !this -> version_title.empty())
-				ss << this -> version_title << this -> version;
-			else ss << this -> version;
+			if ( !this -> info.version_title.empty())
+				ss << this -> info.version_title << this -> info.version;
+			else ss << this -> info.version;
 
 		}
 
-	} else if ( !this -> version.empty()) {
+	} else if ( !this -> info.version.empty()) {
 
-		std::string t = this -> version_title;
+		std::string t = this -> info.version_title;
 		while ( !t.empty() && ( std::isspace(t.front()) || t.front() == '\n' || t.front() == '\r' ))
 			t.erase(0, 1);
 
 		if ( !t.empty())
-			ss << t << this -> version;
-		else ss << this -> version;
+			ss << t << this -> info.version;
+		else ss << this -> info.version;
 	}
 
 	return ss.str();
@@ -125,84 +179,19 @@ std::string usage_t::version_info() const {
 
 usage_t::result_t usage_t::operator [](const std::string& name) const {
 
-	result_t res;
-	auto it = this -> options.find(name);
-
-	if ( name.empty() || it == this -> options.end())
-		return res;
-
-	for ( size_t i = 0; i < args.size(); i++ ) {
-
-		std::string arg = this -> args[i];
-
-		if ( arg.front() == '-' )
-			arg.erase(0, arg.substr(0, 2) == "--" ? 2 : 1 );
-
-		if ( arg == it -> second.key || arg == it -> second.word ) {
-
-			res.enabled = true;
-
-			if ( it -> second.needs_arg && i < args.size() - 1 )
-				res.value = args[i + 1];
-
-			return res;
-		}
-	}
-
-	return res;
+	auto values = this -> values();
+	if ( values.contains(name))
+		return { .enabled = true, .value = values[name] };
+	else return { .enabled = false };
 }
 
 usage_t::result_t usage_t::operator [](const char* name) const {
-
-	return this -> operator[](std::string(name));
+	return this -> operator [](std::string(name));
 }
 
-void usage_t::validate() const {
-
-	if ( args.empty())
-		return;
-
-	std::vector<std::string> args_found;
-
-	size_t i = 0;
-	while ( i < args.size()) {
-
-		std::string key = "";
-		std::string arg = this -> args[i];
-
-		if ( arg.front() == '-' )
-			arg.erase(0, arg.substr(0, 2) == "--" ? 2 : 1 );
-
-		if ( arg.empty())
-			throw std::runtime_error("invalid option " + this -> args[i]);
-
-		auto it = this -> options.end();
-
-		for ( auto _it = this -> options.begin(); _it != this -> options.end(); _it++ ) {
-
-			if ( arg == _it -> second.key || arg == _it -> second.word ) {
-				it =_it;
-				break;
-			}
-		}
-
-		if ( it == options.end())
-			throw std::runtime_error("unknown option " + this -> args[i]);
-		else if ( it -> first.empty()) continue;
-
-		if ( std::find(args_found.begin(), args_found.end(), it -> first) != args_found.end())
-			throw std::runtime_error("duplicate " + this -> args[i] + " argument");
-		else args_found.push_back(it -> first);
-
-		if ( it -> second.needs_arg ) {
-
-			i++;
-			if ( i >= args.size())
-				throw std::runtime_error("option " + this -> args[this -> args.size() - 1] + " requires argument");
-		}
-
-		i++;
-	}
+std::string usage_t::value(const std::string& name) const {
+	auto values = this -> values();
+	return values.contains(name) ? values[name] : "";
 }
 
 std::string usage_t::help() const {
@@ -234,7 +223,8 @@ std::string usage_t::help() const {
 		if ( !it -> second.key.empty()) i += 1 + it -> second.key.size();
 		if ( !it -> second.key.empty() && !it -> second.word.empty()) i += 2;
 		if ( !it -> second.word.empty()) i += 2 + it -> second.word.size();
-		if ( it -> second.needs_arg ) i += 1 + 2 + ( it -> second.arg_name.empty() ? 3 : it -> second.arg_name.size());
+		//if ( it -> second.needs_arg ) i += 1 + 2 + ( it -> second.arg_name.empty() ? 3 : it -> second.arg_name.size());
+		if ( it -> second.flag != usage_t::arg_flag::NO ) i += 1 + 2 + ( it -> second.name.empty() ? 3 : it -> second.name.size());
 
 		if ( ms < i ) ms = i;
 	}
@@ -259,8 +249,10 @@ std::string usage_t::help() const {
 		if ( !it -> second.word.empty())
 			row += "--" + it -> second.word;
 
-		if ( it -> second.needs_arg )
-			row += " <" + ( it -> second.arg_name.empty() ? "arg" : it -> second.arg_name ) + ">";
+		if ( it -> second.flag == usage_t::arg_flag::REQUIRED )
+			row += " <" + ( it -> second.name.empty() ? "arg" : it -> second.name ) + ">";
+		else if ( it -> second.flag == usage_t::arg_flag::OPTIONAL )
+			row += " [" + ( it -> second.name.empty() ? "arg" : it -> second.name ) + "]";
 
 		while ( row.size() < ms )
 			row += ' ';
@@ -273,6 +265,9 @@ std::string usage_t::help() const {
 		s += row;
 	}
 
+	if ( !this -> info.footer.empty())
+		s += "\n" + this -> info.footer;
+
 	return s;
 }
 
@@ -281,7 +276,7 @@ usage_t::operator std::string() const {
 	return this -> title();
 }
 
-std::ostream& operator <<(std::ostream& os, const args_t& args) {
+std::ostream& operator <<(std::ostream& os, const usage_t::args_t& args) {
 
 	os << args.cmd();
 
